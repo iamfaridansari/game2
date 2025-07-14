313b35a73d695b0c97b5d973c87859441e6e5b52
Farid Ansari <iamfaridansari@Farids-MacBook-Air.local>
2025-07-14 22:39:52 +0530

    appbar update

--- js/appbar.js
+++ js/appbar.js
@@ -3,5 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((res) => res.text())
     .then((data) => {
       document.getElementById("app-bar").innerHTML = data;
+
+      const menu = document.querySelectorAll(".menu-btn");
+      const appBarDiv = document.getElementById("app-bar-div");
+
+      menu.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          appBarDiv.classList.toggle("active");
+        });
+      });
     });
 });
