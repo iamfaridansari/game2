40139c7be40ec27ea4346dada354e358f9eb331f
Farid Ansari <iamfaridansari@Farids-MacBook-Air.local>
2025-07-14 19:58:56 +0530

    day2

--- js/chatbar.js
+++ js/chatbar.js
@@ -2,6 +2,31 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("./components/chatbar.html")
     .then((res) => res.text())
     .then((data) => {
-      document.getElementById("chat-bar").innerHTML = data;
+      document.getElementById("chat-bar-section").innerHTML = data;
+
+      //
+      fetch("../json/chatbar.json")
+        .then((res) => res.json())
+        .then((chatData) => {
+          const chatBar = document.getElementById("chat-bar");
+          chatBar.innerHTML = "";
+
+          chatData.forEach((item) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <div class="profile-picture">
+                  <img src="${item.img}" alt="${item.name}" />
+                </div>
+                <div>
+                  <p class="fw-bold yellow-text">${item.name}</p>
+                  <p>${item.message}</p>
+                </div>
+      `;
+            chatBar.appendChild(li);
+          });
+        })
+        .catch((err) => {
+          console.error("Failed to load chat data:", err);
+        });
     });
 });
