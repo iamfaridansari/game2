0c75ae9de543052b46a4bae90bc6670024464a50
Farid Ansari <iamfaridansari@Farids-MacBook-Air.local>
2025-07-20 16:17:01 +0530

    top bonus

---
+++ js/bonus.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("../json/bonus.json")
+    .then((res) => res.json())
+    .then((data) => {
+      //
+      data.slice(0, 3).forEach((item) => {
+        const el = document.createElement("div");
+        el.classList.add("top-bonus-card", "glass");
+        el.innerHTML = `
+        <img src="${item.image}" alt="" />
+            <div>
+              <h2>${item.title}</h2>
+              <p>${item.subtitle}</p>
+            </div>
+            <div
+              class="d-flex flex-wrap align-items-center justify-content-center gap-2"
+            >
+              <button class="button glass icon-button">
+                code: 7noir <i class="fa-solid fa-copy"></i>
+              </button>
+              <button class="button yellow-button">claim now</button>
+            </div>
+        `;
+        document.getElementById("top-bonus").appendChild(el);
+      });
+    });
+});
