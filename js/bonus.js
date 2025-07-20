ba2cd0cf166184463735b19726f264e671d5e11b
Farid Ansari <iamfaridansari@Farids-MacBook-Air.local>
2025-07-20 16:35:06 +0530

    update

--- js/bonus.js
+++ js/bonus.js
@@ -6,10 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
       data.slice(0, 3).forEach((item) => {
         const el = document.createElement("div");
         el.classList.add("top-bonus-card", "glass");
+        el.style.border = `1px solid var(--${item.color})`;
         el.innerHTML = `
         <img src="${item.image}" alt="" />
             <div>
-              <h2>${item.title}</h2>
+              <h2 class="${item.color}-text">${item.title}</h2>
               <p>${item.subtitle}</p>
             </div>
             <div
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
               <button class="button glass icon-button">
                 code: 7noir <i class="fa-solid fa-copy"></i>
               </button>
-              <button class="button yellow-button">claim now</button>
+              <button class="button ${item.color}-button">claim now</button>
             </div>
         `;
         document.getElementById("top-bonus").appendChild(el);
