02e534efe5fc12ce1bbdda1b85d14f967c0fe101
Farid Ansari <iamfaridansari@Farids-MacBook-Air.local>
2025-07-20 17:20:06 +0530

    update

--- js/bonus.js
+++ js/bonus.js
@@ -17,12 +17,34 @@ document.addEventListener("DOMContentLoaded", () => {
               class="d-flex flex-wrap align-items-center justify-content-center gap-2"
             >
               <button class="button glass icon-button">
-                code: 7noir <i class="fa-solid fa-copy"></i>
+                code: <span class="${item.color}-text">7noir</span> <i class="fa-solid fa-copy ${item.color}-text"></i>
               </button>
               <button class="button ${item.color}-button">claim now</button>
             </div>
         `;
         document.getElementById("top-bonus").appendChild(el);
+      });
+      //
+      data.slice(3, data.length).forEach((item) => {
+        const el = document.createElement("div");
+        el.classList.add("top-bonus-card", "glass");
+        el.style.border = `1px solid var(--${item.color})`;
+        el.innerHTML = `
+        <img src="${item.image}" alt="" />
+            <div>
+              <h2 class="${item.color}-text">${item.title}</h2>
+              <p>${item.subtitle}</p>
+            </div>
+            <div
+              class="d-flex flex-wrap align-items-center justify-content-center gap-2"
+            >
+              <button class="button glass icon-button">
+                code: <span class="${item.color}-text">7noir</span> <i class="fa-solid fa-copy ${item.color}-text"></i>
+              </button>
+              <button class="button ${item.color}-button">claim now</button>
+            </div>
+        `;
+        document.getElementById("bottom-bonus").appendChild(el);
       });
     });
 });
